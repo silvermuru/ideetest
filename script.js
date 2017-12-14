594a8241cf2a764b39d5cbeba036cb265a6ca5d5
Martin Nõukas <martinez966@gmail.com>
2017-12-14 14:01:31 +0200

    Test now ready

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 $('.quizz').on('click', function(e) {
   e.preventDefault();
+  setTimeout(function () {
+    $('html, body').animate({
+        scrollTop: $("#quiz").offset().top
+    }, 2000);
+  }, 100);
 });
 
 $('.ending').on('click', function(e) {
   e.preventDefault();
+  setTimeout(function () {
+    $('html, body').animate({
+        scrollTop: $("#ending").offset().top
+    }, 2000);
+  }, 100);
 });
